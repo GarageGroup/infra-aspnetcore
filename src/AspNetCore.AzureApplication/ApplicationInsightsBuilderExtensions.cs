cbe3e94856a714f3fc7633b4d2d883aa244db2f6
pmosk <pm@ggroupp.ru>
2023-12-23 02:04:39 +0400

    Remove AspNetCore.ApplicationInsights

---
+++ src/AspNetCore.AzureApplication/ApplicationInsightsBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Extensions.Hosting;
+
+public static class ApplicationInsightsBuilderExtensions
+{
+    public static IHostBuilder ConfigureApplicationInsights(this IHostBuilder hostBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(hostBuilder);
+        return hostBuilder.ConfigureServices(ConfigureTelemetryServices).ConfigureLogging(ConfigureApplicationInsights);
+    }
+
+    private static void ConfigureApplicationInsights(HostBuilderContext context, ILoggingBuilder builder)
+    {
+        if (context.HasApplicationInsightsConnectionString() is false)
+        {
+            return;
+        }
+
+        builder.AddApplicationInsights();
+    }
+
+    private static void ConfigureTelemetryServices(HostBuilderContext context, IServiceCollection services)
+    {
+        if (context.HasApplicationInsightsConnectionString() is false)
+        {
+            return;
+        }
+
+        services.AddApplicationInsightsTelemetry();
+    }
+
+    private static bool HasApplicationInsightsConnectionString(this HostBuilderContext context)
+        =>
+        string.IsNullOrEmpty(context.Configuration["ApplicationInsights:ConnectionString"]) is false ||
+        string.IsNullOrEmpty(context.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]) is false;
+}
